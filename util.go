5e7a0563e928dcd33820ea6fd577afdcbdb0eae5
jq <jq.go@qq.com>
2022-09-08 18:24:22 +0800

    rem tmpl.go

--- util.go
+++ util.go
@@ -5,6 +5,20 @@ import (
 	"runtime"
 )
 
+var HtmlTmpl = `
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<pre>%s</pre>
+<pre>%s</pre>
+
+</body>
+</html>
+`
+
 func TraceId() (traceId string, ok bool) {
 	_, f, l, ok := runtime.Caller(2)
 	if !ok {
